75089f29119545eef9266ffa018e61de5279cc3c
prasanthaL <prasantha.trivextech@gmail.com>
2026-03-05 17:42:26 +0530

    add

--- app/admin/clients/page.tsx
+++ app/admin/clients/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { prisma } from "@/lib/prisma";
 import AdminShell from "@/components/admin/AdminShell";
 import AddClientForm from "@/components/admin/AddClientForm";
